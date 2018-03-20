d2feb39d26b3df53b4a5bc86d878f597e08f6a4a
Omo Agbagbara <joomla@deltastateonline.com>
2018-03-20 21:00:59 +1000

    using rows

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var bodyParser = require('body-parser')
 
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
@@ -18,15 +17,11 @@ app.get('/alerts', function(req, res){
 });
 
 app.post('/alert', function(req, res){
-	var message  = req.body;
 	
-	var d = new Date()
-	var output_string = message.alert + " - "+ d.toString();
-	//console.log(output_string);
-	
-	
-	io.emit('chat message', output_string);
-	
+	var alertObject  = req.body;	// already an object
+	var d = new Date();	
+	alertObject.recieved = d.toString();		
+	io.emit('new-alert', JSON.stringify(alertObject));	
 	res.end(JSON.stringify( {"message":"Recieved", "when":d.toString()}) );
 })
 
@@ -35,11 +30,6 @@ io.on('connection', function(socket){
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
-  /*
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-	io.emit('chat message', msg);	
-  });*/
 });
 
 
